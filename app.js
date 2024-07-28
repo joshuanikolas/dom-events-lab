afb2106ad8f615a576e238cc3b317dfc2530c558
joshuanikolas <nikolassnchez@icloud.com>
2024-07-28 18:24:38 -0500

    variables complete

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const display = calculator.querySelector('.display');
 const buttons = calculator.querySelectorAll('.button');
 
 /*-------------------------------- Variables --------------------------------*/
-
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
